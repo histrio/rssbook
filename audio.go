0636e180c9a2fb23721b8c95578d315281f6fd0b
Rinat Sabitov <rinat.sabitov@gmail.com>
2017-01-21 00:40:04 +0300

    Some refactoring

---
+++ audio.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"path"
+	"strconv"
+	"strings"
+	"time"
+)
+
+// Merge all audio files listed in file into one
+func mergeFiles(listFileName string) string {
+	infoLog.Println("Merging")
+	mergedFile, err := ioutil.TempFile(os.TempDir(), "prefix")
+	check(err)
+	mergedFilename := mergedFile.Name() + ".mp3"
+	simpleExec("ffmpeg", "-y", "-f", "concat", "-safe", "0", "-i", listFileName, "-c", "copy", mergedFilename)
+	return mergedFilename
+}
+
+// Calculate duration of audio file
+func getDuration(filename string) time.Time {
+	durationRaw := simpleExec("ffprobe", "-i", filename, "-show_entries", "format=duration", "-v", "quiet", "-of", "csv")
+	duration := strings.Split(durationRaw, ",")[1]
+	durationS := strings.Split(duration, ".")
+	seconds, err := strconv.ParseInt(strings.TrimSpace(durationS[0]), 10, 64)
+	check(err)
+	nseconds, err := strconv.ParseInt(strings.TrimSpace(durationS[1]), 10, 64)
+	check(err)
+	t0 := time.Time{}
+	t0 = t0.Add(time.Second * time.Duration(seconds))
+	t0 = t0.Add(time.Nanosecond * time.Duration(nseconds))
+	infoLog.Println("Duration: " + formatTime(t0))
+	return t0
+}
+
+func split(book bookMeta, skip time.Time, limit time.Time) string {
+	fname := fmt.Sprintf("%v%02d%02d%02d.mp3", "part", skip.Hour(), skip.Minute(), skip.Second())
+	fpath := path.Join(book.dst, fname)
+
+	t1s := formatTime(skip)
+	s1s := formatTime(limit)
+	infoLog.Println("Split " + t1s)
+
+	splitedFile, err := ioutil.TempFile(os.TempDir(), "rssbook-")
+	check(err)
+
+	splitedFilename := splitedFile.Name() + ".mp3"
+	defer os.Remove(splitedFilename)
+
+	titleMetadata := fmt.Sprintf("title=%v", book.title)
+
+	simpleExec("ffmpeg", "-y", "-i", book.src, "-metadata", titleMetadata, "-acodec", "copy", "-t", s1s, "-ss", t1s, splitedFilename)
+	simpleExec("lame", "-V", "9", "--vbr-new", "-mm", "-h", "-q", "0", "-f", splitedFilename, fpath)
+	return fpath
+}
---
+++ utils.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+	"os/exec"
+	"path"
+	"path/filepath"
+	"time"
+)
+
+func getid(domain string, link string, date time.Time) string {
+	dateFormatted := fmt.Sprintf("%d-%02d-%02d", date.Year(), date.Month(), date.Day())
+	return fmt.Sprintf("tag:%v,%v:%v", domain, dateFormatted, link)
+}
+
+func getFileSize(fn string) int64 {
+	file, err := os.Open(fn)
+	check(err)
+	fi, err := file.Stat()
+	check(err)
+	return fi.Size()
+}
+
+func check(e error) {
+	if e != nil {
+		log.Fatalf("%s\n", e)
+	}
+}
+
+func simpleExec(name string, arg ...string) string {
+	cmd := exec.Command(name, arg...)
+	output, err := cmd.CombinedOutput()
+	if err != nil {
+		fmt.Println(fmt.Sprint(err) + ": " + string(output))
+		return "Error"
+	}
+	return string(output)
+}
+
+func formatTime(t time.Time) string {
+	return fmt.Sprintf("%02d:%02d:%02d", t.Hour(), t.Minute(), t.Second())
+}
+
+// Collects files from folder to merge
+func getFileList(dir string) string {
+	files, err := ioutil.ReadDir(dir)
+	check(err)
+
+	listFile, err := ioutil.TempFile(os.TempDir(), "prefix")
+	defer listFile.Close()
+	check(err)
+
+	for _, f := range files {
+		fname := f.Name()
+		ext := filepath.Ext(fname)
+		if ext == ".mp3" {
+			listFile.WriteString(fmt.Sprintf("file '%v'\n", path.Join(dir, fname)))
+		}
+	}
+	return listFile.Name()
+}
